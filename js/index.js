cf703a7a2b3eced30e2756f8e78baaeaad6ff40f
Heorhi Kartavenko <SocialImp@mail.ru>
2023-10-30 21:24:29 +0300

    :feat: add slider

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
-document.getElementById('myImage').addEventListener('mouseover', function() {
-    this.classList.add('grayscale');
-   });
-   
-   document.getElementById('myImage').addEventListener('mouseout', function() {
-    this.classList.remove('grayscale');
-   });
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  slide();
+});
+
+nextButton.addEventListener('click', () => {
+  slideIndex = (slideIndex + 1) % slideCount;
+  slide();
+});
+
+const slide = () => {
+  const imageWidth = slider.clientWidth;
+  const slideOffset = -slideIndex * imageWidth;
+  slider.style.transform = `translateX(${slideOffset}px)`;
+}
+
+window.addEventListener('load', () => {
+  slide();
+});
